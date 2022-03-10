2ca3947e53082db368b54578ff2d81228a892157
Lukáš Paukert <paukert.lukas@gmail.com>
2022-03-10 21:36:50 +0100

    Add responsive tables! :O

---
+++ public/js/table.js
@@ -0,0 +1,68 @@
+function processAction(row) {
+    if (window.innerWidth > 991) {
+        return;
+    }
+    row.dataset.collapsed === 'true' ? expandRow(row) : collapseRow(row);
+}
+
+function expandRow(row) {
+    row.dataset.collapsed = 'false';
+    let newRow = document.createElement('tr');
+    newRow.innerHTML = getRowContent(row);
+    row.parentElement.insertBefore(newRow, row.nextElementSibling);
+    row.querySelector('.arrow').classList.replace('fa-angle-down', 'fa-angle-up');
+}
+
+function collapseRow(row) {
+    row.dataset.collapsed = 'true';
+    row.nextElementSibling.remove();
+    row.querySelector('.arrow').classList.replace('fa-angle-up', 'fa-angle-down');
+}
+
+const rows = Array.from(document.getElementsByTagName('tr'));
+for (let i = 0; i < rows.length; i++) {
+    rows[i].firstElementChild.addEventListener('click', () => processAction(rows[i]));
+}
+
+window.addEventListener('resize', function () {
+    if (window.innerWidth < 992) {
+        return;
+    }
+
+    for (let i = 0; i < rows.length; i++) {
+        if (rows[i].dataset.collapsed === 'false') {
+            collapseRow(rows[i]);
+        }
+    }
+});
+
+function getRowContent(row) {
+    const level = row.querySelector('.level');
+    const deadline = row.querySelector('.deadline');
+    const nearDeadline = deadline.classList.contains('table-danger');
+    return `
+        <td colspan="6">
+            <ul>
+                ${level ? `
+                    <li class="d-md-none">
+                        <span class="title">Typ:</span>
+                        ${level.innerText}
+                    </li>
+                    ` : ``
+                }
+                <li class="d-sm-none">
+                    <span class="title">Přihlášky do:</span>
+                    ${nearDeadline ? `<span class="nearDeadline">${deadline.innerText}</span>` : deadline.innerText}
+                </li>
+                <li class="d-lg-none">
+                    <span class="title">Účast:</span>
+                    ${row.querySelector('.attendance').innerText}
+                </li>
+                <li class="d-md-none">
+                    <span class="title">Stav:</span>
+                    ${row.querySelector('.entry-status').innerText}
+                </li>
+            </ul>
+        </td>
+    `;
+}
