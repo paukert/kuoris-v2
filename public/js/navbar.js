93d4fbcd8080262b62317ccf868ae55df54c4942
Lukáš Paukert <paukert.lukas@gmail.com>
2022-03-20 17:25:09 +0100

    Remove cyclic links from navigation

--- public/js/navbar.js
+++
@@ -1,9 +0,0 @@
-const navbarItems = document.querySelectorAll('#navbarNav a');
-
-for (let i = 0; i < navbarItems.length; i++) {
-    if (navbarItems[i].getAttribute('href') === window.location.pathname) {
-        navbarItems[i].classList.add('active');
-        navbarItems[i].setAttribute('aria-current', 'page');
-        break;
-    }
-}
