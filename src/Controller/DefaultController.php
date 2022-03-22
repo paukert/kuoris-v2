8c2809ce80fd980a2df7b57feab91b6ccf0911a7
Lukáš Paukert <paukert.lukas@gmail.com>
2022-03-22 07:56:12 +0100

    Add landing page and new logo

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -18,8 +18,17 @@ class DefaultController extends AbstractController
         $this->eventService = $eventService;
     }
 
-    #[Route('/', name: 'app_homepage')]
-    public function index(): Response
+    #[Route('/', name: 'app_landing')]
+    public function landing(): Response
+    {
+        if ($this->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            return $this->redirectToRoute('app_homepage');
+        }
+        return $this->render('landing.html.twig');
+    }
+
+    #[Route('/home', name: 'app_homepage')]
+    public function home(): Response
     {
         $races = $this->eventService->getEventsWithNearestDeadline(Race::class);
         $trainings = $this->eventService->getEventsWithNearestDeadline(Training::class, 3);
